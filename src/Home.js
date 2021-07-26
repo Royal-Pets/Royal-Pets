b30fee0c6c63bf3f6f6ebcc646a2cf350684aeb9
saleem-ux <saleem_diab86@yahoo.com>
2021-07-26 01:42:51 -0700

    new

--- src/Home.js
+++ src/Home.js
@@ -1,132 +1,156 @@
-import React, { Component } from "react";
-import { Container, Row, Carousel, Modal, Button, Card } from "react-bootstrap";
-import img1 from "./Images/download.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import { withAuth0 } from "@auth0/auth0-react";
+import React, { Component } from 'react';
+import {
+    Container, Row, Carousel, Modal, Button, Card
+} from 'react-bootstrap';
+import img1 from './Images/slide2.jpg';
+import img2 from './Images/dogs-and-cats-1-638.webp';
+import img3 from './Images/slide3.jpg';
+import imgCard from './Images/cardimage1.jpg';
+import imgCard2 from './Images/cardimage2.jpg';
+import imgCard3 from './Images/cardimage3.jpg';
+import imgCard4 from './Images/cardimage4.jpg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        }
+    }
 
-  handleModal() {
-    this.setState({ show: !this.state.show });
-  }
-  render() {
-    return (
-      <Container fluid className="home-page">
-        <Row className="home-top-background-section">
-          <Carousel>
-            <Carousel.Item interval={1000}>
-              <img className="d-block w-100" src={img1} alt="First slide" />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-              <img className="d-block w-100" src={img1} alt="Second slide" />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block w-100" src={img1} alt="Third slide" />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </Row>
-        <Row>
-          <div className="home-top-section">
-            <h1 className="home-title">We Have Everything Your Pet Needs.</h1>
-          </div>
-        </Row>
-        <Row className="home-video-section">
-          <Button
-            style={{ width: "7rem" }}
-            variant="primary"
-            onClick={() => {
-              this.handleModal();
-            }}
-          >
-            <FontAwesomeIcon icon={faPlay} />
-          </Button>
-          <Modal
-            show={this.state.show}
-            className="home-video-modal"
-            onHide={() => this.handleModal()}
-          >
-            <Modal.Body>
-              <iframe
-                width="465"
-                height="315"
-                src="https://www.youtube.com/embed/8o_myFa_hzo"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </Modal.Body>
-          </Modal>
-        </Row>
-        <Row>
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title>Our History</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title>Our Products</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title>About Us</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title>Our Vision</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Row>
-      </Container>
-    );
-  }
+    handleModal() {
+        this.setState({ show: !this.state.show })
+    }
+      
+
+    render() {
+        return (
+            <Container fluid className="home-page">
+                <Row className="home-top-background-section">
+                    <Carousel className='slide-show'>
+                        <Carousel.Item interval={1000}>
+                            <img height='600'
+                                className="d-block w-100"
+                                src={img1}
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Professional Advice</h3>
+                                <p>Whether it's a quick question over the phone or a second-opinion consultation, we're here & waiting</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item interval={500}>
+                            <img height='600'
+                                className="d-block w-100"
+                                src={img2}
+                                alt="Second slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Your Pet Comes First</h3>
+                                <p>Healthcare from your pet's point of view means we treat your pet as you would like to be treated yourself</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img height='600'
+                                className="d-block w-100"
+                                src={img3}
+                                alt="Third slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Value for Money Healthcare Basics</h3>
+                                <p>Ask about our competitively priced vaccinations, desexes and wellness dentals</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </Row>
+
+                <Row>
+                    <div className="home-top-section">
+                        <h1 className="home-title">We Have Everything Your Pet Needs.</h1>
+                        <p>hi</p>
+                    </div>
+                </Row>
+                <Row>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Img variant="top" src={imgCard} height='180' />
+                            <Card.Title>Island wide Delivery</Card.Title>
+                            <Card.Text>
+                                We cover all cities in Jordan
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Img variant="top" src={imgCard4} height='180' />
+                            <Card.Title>Best Care Buyer Protection</Card.Title>
+                            <Card.Text>
+                                If Goods have Problems
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Img variant="top" src={imgCard3} height='180' />
+
+                            <Card.Title>Secure Payment</Card.Title>
+                            <Card.Text>
+                                100% Secure payments for online transactions
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Img variant="top" src={imgCard2} height='180' />
+                            <Card.Title>24/7 Support</Card.Title>
+                            <Card.Text>
+                                Dedicated support
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row className="home-video-section">
+                    <Button style={{ width: '50rem', height: '30rem' }} variant="primary" onClick={() => { this.handleModal() }} >
+                        <FontAwesomeIcon icon={faPlay} />
+                    </Button>
+                    <Modal show={this.state.show} className="home-video-modal" onHide={() => this.handleModal()}>
+                        <Modal.Body>
+                            <iframe width="465" height="315" src="https://www.youtube.com/embed/BDJ8xyQjyhM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </Modal.Body>
+                    </Modal>
+                </Row>
+                <div>
+                    <h2>Our Values</h2>
+                    <p>Everything we do at Royal-Pets – from the way we greet you on the phone, to the way we introduce a new service or train a new team member, is underpinned by the following seven core values:
+                        <br></br>
+                        ★ Excellence ★ Care ★ Innovation ★ Professionalism ★ Consistency ★ Enjoyment ★ Value for money ★
+                    </p>
+                    <br></br>
+                    <h2>Our Mission</h2>
+                    <p>To provide innovative, high-quality, consistent, value-for-money, pet friendly health care that will make your pet want to come back again…and again.</p>
+                    <br></br>
+                    <h2>Our Motto</h2>
+                    <p>…is “Health care from your pet’s point of view.”</p>
+                    <br></br>
+                    <h2>Our Vision</h2>
+                    <p>5 years from now, Pet Universe will be the ultimate professional veterinary practice that combines excellent healthcare delivered in a consistent, innovative, enjoyable, caring and pet-friendly way to create a value-for-money yet profitable and unforgettable service experience from our:
+                        <br></br>
+                        ★ Pet’s Point of View ★ Client’s Point of View ★ Staff’s point of view ★ Platinum Partner’s Point Of View ★ Owner’s Point of View ★
+                    </p>
+
+                </div>
+
+            </Container>
+        )
+    }
 }
 
-export default withAuth0(Home);
+export default Home
